20a94411f0f53615958a56f4af0f5600dd436826
haseebn19 <haseeb.kn@outlook.com>
2023-08-07 01:51:52 -0400

    fixed some gitignore names
    implemented private messaging for users
    who set their visibility to private
    
    renamed events to user_data
    
    implemented settings command group
    
    created settings command group
    implemented private calendar lists

---
+++ settings_group.py
@@ -0,0 +1,30 @@
+from lib import *
+
+
+def setup(bot: commands.Bot):
+    settings = bot.create_group(name="settings")
+
+    @settings.command(name="visibility", description="Set your visibility settings")
+    async def visibility(
+        ctx: commands.Context,
+        visibility: discord.commands.Option(
+            str,
+            "Choose visibility setting",
+            choices=[
+                discord.commands.OptionChoice(name="public", value="public"),
+                discord.commands.OptionChoice(name="private", value="private"),
+            ],
+        ),
+    ):
+        await ctx.defer()
+
+        user_id = str(ctx.author.id)
+        user_data = bot.user_data_handler.load_user_data(user_id)
+
+        # Set the visibility in the user's data
+        user_data["visibility"] = visibility
+        bot.user_data_handler.save_user_data(user_id, user_data)
+
+        await ctx.edit(content=f"Your visibility setting has been set to {visibility}.")
+
+    print("Settings group loaded")
